ed25420fc5127f855a8fa632e203ee331daaaa7c
Tasilimy <tasilimykaba@gmail.com>
2025-11-03 18:41:44 +0100

    ajout du 2eme test renvoie 0 si la base vaut 0

--- test/triangle.test.js
+++ test/triangle.test.js
@@ -2,8 +2,13 @@ const Triangle = require('../src/triangle').triangle;
 const expect = require('chai').expect;
 
 describe('Triangle', function() {
-  it('calcule correctement l\'aire', function() {
+  it('calcule correctement l\'aire pour 4x6', function() {
     const t = new Triangle(4, 6);
     expect(t.getArea()).to.equal(12); // 4*6/2 = 12
+  });
+
+  it('renvoie 0 si la base vaut 0', function() {
+    const t = new Triangle(0, 10);
+    expect(t.getArea()).to.equal(0);
   });
 });
